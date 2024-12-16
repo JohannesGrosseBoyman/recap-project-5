d23d66a794ac2b7a681589acf075b49a80ab39b8
JohannesGrosseBoymann <dohecaf1219@gmail.com>
2024-12-16 11:01:42 +0100

    add navigation component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import GlobalStyle from "../styles";
 import useSWR from "swr";
+import Navigation from "@/components/Navigation";
 
 const url = "https://example-apis.vercel.app/api/art";
 const fetcher = (url) => fetch(url).then((response) => response.json());
@@ -17,6 +18,7 @@ export default function App({ Component, pageProps }) {
     <>
       <GlobalStyle />
       <Component {...pageProps} data={data} />
+      <Navigation />
     </>
   );
 }
